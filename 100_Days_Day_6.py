239c82a5f5f364efc0a3cd9df1d0d8ad149bad08
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-02 14:26:15 -0500

    Add files via upload
    Starting the project...

---
+++ 100_Days_Day_6.py
@@ -0,0 +1,12 @@
+#####################
+# Escaping the Maze #
+#####################
+
+# Will need to run code at https://reeborg.ca/index_en.html
+
+
+# def my_function():
+#     print("Hello!")
+#     print("Farewell!")
+
+# my_function()
