ec18a01d7c24b60c7f8348544eeea6ca7f4b59d0
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-05-04 14:19:51 -0500

    Add files via upload
    Finished the instructions for the robot.

--- 100_Days_Day_6.py
+++ 100_Days_Day_6.py
@@ -2,11 +2,33 @@
 # Escaping the Maze #
 #####################
 
-# Will need to run code at https://reeborg.ca/index_en.html
+# IMPORTANT:
+# You will need to run this code for the robot's maze at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json.
+# Otherwise, this program will be useless.
 
+# Function to turn robot right.
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
-# def my_function():
-#     print("Hello!")
-#     print("Farewell!")
+# Function to move robot in the correct direction.
+def move_forward():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+            move()
 
-# my_function()
+# As long as robot is not yet at the goal, robot will keep moving.
+while not at_goal():
+    move_forward()
+
+# The end of Reeborg's maze!
